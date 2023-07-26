3ac48ff5231354e679e4a538d42b428efe7ff9d6
DauntlessK <sunexcedent@yahoo.com>
2023-07-26 11:44:43 -0400

    6.8

--- main.py
+++ main.py
@@ -201,6 +201,8 @@ def getHitorStand(myHand, Dealer):
     Returns hit or stand string
     Table has 1 for hit and 0 for stand, dealer showing at top row, our total on side"""
 
+    dealerAdjustedValue = dealer.total - 3 #need to offset dealer totals by 3 because columns start at 2, not 0
+
     #dealer:  2  3  4  5  6  7  8  9 10 11
     table = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  #0   notused
              [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],  #1   notused
@@ -225,10 +227,15 @@ def getHitorStand(myHand, Dealer):
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  #20
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]  #21
 
-    col4 = table[4]
-    col5 = table[5]
-    col16 = table[16]
-    print(col16[8])
+    col = []
+    for x in range(22):
+        col.append(table[x])
+    #print(col[17][0])
+    val = col[myHand.total][dealerAdjustedValue]
+    if val == 0:
+        return "Stand"
+    else:
+        return "Hit"
 
 def playBlackJackLoop(numDecks, reshuf):
     """Loop that gets user input to continually play blackjack. Keeps track of wins, losses and ties for the session."""
@@ -313,8 +320,16 @@ def simulation(gamesToSim, numDecks, reshuf):
     threecard_losses = 0
     threecard_tieValues = ["Three Card Ties", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     threecard_ties = 0
+    secondHand_winValues = ["Second Hand Wins", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    secondHand_wins = 0
+    secondHand_lossValues = ["Second Hand Losses", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    secondHand_losses = 0
+    secondHand_tieValues = ["Second Hand Ties", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    secondHand_ties = 0
     deck = Deck(numDecks, reshuf)
-    money = 0     #starting money
+    moneyTwoCard = 0     #starting money for two card
+    moneyThreeCard = 0   #starting money for three card
+    moneySecondHand = 0  #starting money for second hand (that hits/stands according to table)
 
 
     loops = 0
@@ -325,8 +340,12 @@ def simulation(gamesToSim, numDecks, reshuf):
         dealer.discardHand()      # ensures dealer hand values are empty / reset
 
         myHand.newHand(deck, 2)
-        dealer.newHand(deck, 2)
+        myHand2 = myHand()
+        dealer.newHand(deck, 1)
         handStartingValue = myHand.getTotal()
+
+        while getHitorStand(myHand2, dealer) == "Hit" :
+            hyHand2.draw1(deck)
 
         while dealer.getTotal() <= 16:
             dealer.draw1(deck)
@@ -350,7 +369,12 @@ def simulation(gamesToSim, numDecks, reshuf):
         else:
             print("Error")
 
-        myHand.draw1(deck)
+        #if hand2 did take a card, add third card to first hand so that the hands first 3 cards match
+        if len(myHand2.hand) > 2:
+            myHand.hand.append(myHand2.hand[2])
+        else:
+            myHand.draw1(deck)
+            
         # check if player won/lost/tied with 3 cards
         if (myHand.isBust()):            #no matter what if player busts, it is a loss
             threecard_lossValues[handStartingValue] += 1
@@ -405,4 +429,3 @@ def simulation(gamesToSim, numDecks, reshuf):
 
 #playBlackJackLoop(2,20)
 #simulation(1000,5,208)
-getHitorStand()
